3ffd8c83b0f41b9d0049b06600224658af8f7a31
Shadowzzh <shadow1746556951@gmail.com>
2026-01-17 16:22:57 +0800

    chore: add manifest configuration for content scripts

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+	manifest: {
+		content_scripts: [
+			{
+				matches: ["<all_urls>"],
+			},
+		],
+	},
 	modules: ["@wxt-dev/module-react"],
 })
